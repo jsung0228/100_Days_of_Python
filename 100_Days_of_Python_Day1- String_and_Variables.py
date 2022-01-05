468b27765028860350d6f0a507bd0c795946697c
jsung0228 <jsung0228@gmail.com>
2022-01-05 21:40:23 +0900

    Initial commit

---
+++ 100_Days_of_Python_Day1- String_and_Variables.py
@@ -0,0 +1,31 @@
+print("Hello World!")
+
+# How can we use a single print to create a new line?
+print("Hello World!\nHello World!")
+
+# Concatenating two strings
+print('Hello' + 'Angela')
+
+# How do we add a space in between?
+print('Hello' + " " + "Angela")
+
+#  if the user want to enter something?
+# input("What is your name?") #There will be a cursor because it expects something
+# print("Hello " + input("What is your name?") + "!")
+
+# Input excercise
+# Remember to visualize the steps of how the computer will read the code
+
+print(len(input("What is your name? ")))
+
+# Naming variables for
+user_name = input("What is your name?")
+length = len(user_name)
+print(user_name)
+
+#Band name generator
+print("Welcome to the band name generator.")
+city = input("What city did your grow up in? \n")
+pet = input("What is your pet name? \n")
+print("Your band name is " + city + " " + pet)
+
